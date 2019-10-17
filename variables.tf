a6768d3f0cae4a821af1e85927f5ea4648754a88
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2019-10-17 13:27:23 -0600

    roll back default ubuntu version to xenial (#28)
    re Issue https://github.com/hashicorp/terraform-azurerm-terraform-enterprise/issues/21
    default build should work

--- variables.tf
+++ variables.tf
@@ -227,7 +227,7 @@ variable "storage_image" {
   default = {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 }
