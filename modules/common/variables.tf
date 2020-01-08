72eac8be3ec0aa63ed00ada96db1250b7fdc164b
Brandon Ferguson <bnferguson@hashicorp.com>
2020-01-08 17:42:50 +0100

    Update another clobbered change in merge

--- modules/common/variables.tf
+++ modules/common/variables.tf
@@ -74,7 +74,7 @@ locals {
   private_key_filename    = "${local.ssh_public_key_path}/${local.key_name}.priv"
   prefix                  = "${var.resource_prefix}-${var.install_id}"
   rendered_kv_rg_name     = coalesce(var.key_vault["rg_name"], var.rg_name)
-  rendered_domain_rg_name = coalesce(var.domain_rg_name, var.rg_name)
+  rendered_domain_rg_name = coalesce(var.dns["rg_name"], var.rg_name)
   
   default_tags = {
     Application = "Terraform Enterprise"
