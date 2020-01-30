5d0f430b2ca864af680ab0d7bc93c804936e822d
Amy Brown <amy@hashicorp.com>
2020-01-30 15:04:26 -0600

    Fix install script issue (again)
    original fix was overwritten somehow.

--- modules/configs/files/install-ptfe.sh
+++ modules/configs/files/install-ptfe.sh
@@ -19,11 +19,10 @@ if [ -f /etc/redhat-release ]; then
   mkdir -p /lib/tc
   mount --bind /usr/lib64/tc/ /lib/tc/
   sed -i -e 's/^SELINUX=enforcing/SELINUX=permissive/' /etc/sysconfig/selinux
-  sed -i -e '/rhui-REGION-rhel-server-extras/,/^$/s/enabled=0/enabled=1/g'  /etc/yum.repos.d/redhat-rhui.repo
-  yum -y install docker wget jq chrony ipvsadm unzip
-  # remove after testing rhui?
-  # curl -sfSL -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
-  # chmod +x /usr/bin/jq
+  #sed -i -e '/rhui-REGION-rhel-server-extras/,/^$/s/enabled=0/enabled=1/g'  /etc/yum.repos.d/redhat-rhui.repo
+  yum -y install docker wget chrony ipvsadm unzip
+  curl -sfSL -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+  chmod +x /usr/bin/jq
   systemctl enable docker
   systemctl start docker
 else
