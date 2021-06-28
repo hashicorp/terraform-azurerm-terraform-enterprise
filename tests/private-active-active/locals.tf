b7e459de0d326f5b7153bb22f8a84d4900a1fca1
anniehedgpeth <annie@hashicorp.com>
2021-06-28 11:56:25 -0600

    correct tags

--- tests/private-active-active/locals.tf
+++ tests/private-active-active/locals.tf
@@ -1,8 +1,8 @@
 locals {
   common_tags = {
     Terraform   = "cloud"
-    Environment = "${local.friendly_name_prefix}-test-public-active-active"
-    Description = "Public Active/Active"
+    Environment = "${local.friendly_name_prefix}-test-private-active-active"
+    Description = "Private Active/Active"
     Repository  = "hashicorp/terraform-azurerm-terraform-enterprise"
     Team        = "Terraform Enterprise on Prem"
     OkToDelete  = "True"
