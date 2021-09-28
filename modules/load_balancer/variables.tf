85eadc04e30b716fb46b7de1bfb95830ead31285
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-09-28 13:11:06 -0400

    Remove default from tenant_id

--- modules/load_balancer/variables.tf
+++ modules/load_balancer/variables.tf
@@ -63,7 +63,6 @@ variable "resource_group_name" {
 }
 
 variable "tenant_id" {
-  default     = null
   type        = string
   description = "The Azure Active Directory tenant ID that should be used for authenticating requests to the Key Vault"
 }
