76a8ac71ab20e379fda79fa19609543d65dcbc4f
Annie Hedgpeth <annie@hashicorp.com>
2021-10-12 11:39:29 -0600

    Update tests/standalone-poc/locals.tf
    Co-authored-by: Aaron Lane <10655063+aaron-lane@users.noreply.github.com>

--- tests/standalone-poc/locals.tf
+++ tests/standalone-poc/locals.tf
@@ -1,7 +1,7 @@
 locals {
   common_tags = {
     Terraform   = "False"
-    Environment = "${local.friendly_name_prefix}-test-poc-standalone"
+    Environment = "${local.friendly_name_prefix}-test-standalone-poc"
     Description = "Standalone, Demo-Mode scenario deployed from CircleCI"
     Repository  = "hashicorp/terraform-azurerm-terraform-enterprise"
     Team        = "Terraform Enterprise on Prem"
