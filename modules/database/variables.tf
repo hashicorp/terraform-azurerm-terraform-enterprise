cb54d99a93b0f4a440b8e6c1db9d5d390b08390c
anniehedgpeth <annie@hashicorp.com>
2022-01-06 16:14:31 -0700

    add variable for availability zone for PG

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -20,7 +20,6 @@ variable "resource_group_name" {
 # Database
 # --------
 variable "database_user" {
-  default     = "tfeuser"
   type        = string
   description = "Postgres username"
 }
@@ -50,11 +49,33 @@ variable "database_private_dns_zone_id" {
   description = "The identity of the private DNS zone in which the database will be deployed."
 }
 
-# ----
 variable "database_backup_retention_days" {
-  default     = 7
   type        = number
-  description = "Backup retention days for the PostgreSQL server. Supported values are between 7 and 35 days"
+  description = "Backup retention days for the PostgreSQL server."
+
+  validation {
+    condition = (
+      var.database_backup_retention_days >= 7 &&
+      var.database_backup_retention_days <= 35
+    )
+
+    error_message = "Supported values for database_backup_retention_days are between 7 and 35 days."
+  }
+}
+
+variable "database_availability_zone" {
+  type        = number
+  description = "The Availability Zone of the PostgreSQL Flexible Server."
+
+  validation {
+    condition = (
+      var.database_availability_zone == 1 ||
+      var.database_availability_zone == 2 ||
+      var.database_availability_zone == 3
+    )
+
+    error_message = "Possible values for database_availability_zone are 1, 2 and 3."
+  }
 }
 
 # Tagging
