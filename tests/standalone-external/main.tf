d1ee1cbd21c7c0ab2a52cf3c80f895d416e58289
anniehedgpeth <annie@hashicorp.com>
2022-01-13 16:51:19 -0700

    update standalone-external (need to test)

--- tests/standalone-external/main.tf
+++ tests/standalone-external/main.tf
@@ -26,28 +26,15 @@ module "standalone_external" {
   vm_key_secret             = data.azurerm_key_vault_secret.vm_key
 
   # Standalone Demo Mode Scenario
-  iact_subnet_list     = ["0.0.0.0/0"]
-  vm_node_count        = 1
-  vm_sku               = "Standard_D4_v3"
-  vm_image_id          = "ubuntu"
-  load_balancer_public = true
-  load_balancer_type   = "application_gateway"
-  installation_type    = "production"
-
-  redis = {
-    family                          = "P"
-    sku_name                        = "Premium"
-    size                            = "3"
-    enable_non_ssl_port             = false
-    enable_authentication           = false
-    rdb_backup_enabled              = false
-    rdb_backup_frequency            = null
-    rdb_backup_max_snapshot_count   = null
-    rdb_existing_storage_account    = null
-    rdb_existing_storage_account_rg = null
-    use_tls                         = false
-    minimum_tls_version             = "1.2"
-  }
+  installation_type           = "production"
+  iact_subnet_list            = ["0.0.0.0/0"]
+  vm_node_count               = 1
+  vm_sku                      = "Standard_D4_v3"
+  vm_image_id                 = "ubuntu"
+  load_balancer_public        = true
+  load_balancer_type          = "application_gateway"
+  redis_enable_authentication = false
+  redis_use_tls               = false
 
   create_bastion = false
   tags           = local.common_tags
