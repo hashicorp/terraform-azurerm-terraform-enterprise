a3335d95dffffe67d76c99d3ce2ff7e3829ce644
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-03-16 11:25:29 -0400

    test branch

--- fixtures/test_proxy/main.tf
+++ fixtures/test_proxy/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_key_vault_access_policy" "proxy" {
 }
 
 module "test_proxy_init" {
-  source = "github.com/hashicorp/terraform-random-tfe-utility//fixtures/test_proxy_init?ref=main"
+  source = "github.com/hashicorp/terraform-random-tfe-utility//fixtures/test_proxy_init?ref=utility-proxy-azure"
 
   mitmproxy_ca_certificate_secret = var.mitmproxy_ca_certificate_secret
   mitmproxy_ca_private_key_secret = var.mitmproxy_ca_private_key_secret
