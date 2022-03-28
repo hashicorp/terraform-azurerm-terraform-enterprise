ffd46a77601f950da67a62f88a36c60ad569173e
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-03-28 14:13:32 -0400

    fix

--- fixtures/test_proxy/variables.tf
+++ fixtures/test_proxy/variables.tf
@@ -52,7 +52,7 @@ variable "mitmproxy_ca_certificate_secret" {
   default     = null
   description = <<-EOD
   The identifier of a secret comprising a Base64 encoded PEM certificate file for the mitmproxy Certificate Authority.
-  For GCP, the Terraform provider calls this value the secret_id and the GCP UI calls it the name.
+  For Azure, the secret resides in Azure Key Vault.
   EOD
   type        = string
 }
@@ -61,7 +61,7 @@ variable "mitmproxy_ca_private_key_secret" {
   default     = null
   description = <<-EOD
   The identifier of a secret comprising a Base64 encoded PEM private key file for the mitmproxy Certificate Authority.
-  For GCP, the Terraform provider calls this value the secret_id and the GCP UI calls it the name.
+  For Azure, the secret resides in Azure Key Vault.
   EOD
   type        = string
 }
