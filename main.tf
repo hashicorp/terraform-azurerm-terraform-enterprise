d5143521067cf67a760605cb2778f49a0b0b49f7
John Kerry <john.kerry@hashicorp.com>
2022-03-29 08:56:15 -0400

    removing testing branch references.

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ module "database" {
 # TFE and Replicated settings to pass to the tfe_init module
 # -----------------------------------------------------------------------------
 module "settings" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/settings?ref=ah-add-aws-func"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/settings"
 
   # TFE Base Configuration
   installation_type = var.installation_type
@@ -185,7 +185,7 @@ module "settings" {
 # Azure user data / cloud init used to install and configure TFE on instance(s)
 # -----------------------------------------------------------------------------
 module "tfe_init" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/tfe_init?ref=ah-add-aws-func"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/tfe_init"
 
   # TFE & Replicated Configuration data
   cloud                    = "azurerm"
