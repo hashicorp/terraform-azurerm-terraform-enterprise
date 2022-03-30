b9125e3f72ca5be5a54f13d96a900b2ad9aedb98
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2022-03-30 16:39:40 -0400

    Implement feedback

--- variables.tf
+++ variables.tf
@@ -575,14 +575,14 @@ variable "installation_type" {
 variable "production_type" {
   default     = null
   type        = string
-  description = <<-EOD
-	Where Terraform Enterprise application data will be stored. Valid values are
-	`external`, `disk`, or `null`. Choose `external` when storing application
-	data in an external object storage service and database. Choose `disk` when
-	storing application data in a directory on the Terraform Enterprise instance
-	itself. Leave it `null` when you want Terraform Enterprise to use its own
-	default.
-  EOD
+  description = <<-EOF
+  Where Terraform Enterprise application data will be stored. Valid values are
+  `external`, `disk`, or `null`. Choose `external` when storing application
+  data in an external object storage service and database. Choose `disk` when
+  storing application data in a directory on the Terraform Enterprise instance
+  itself. Leave it `null` when you want Terraform Enterprise to use its own
+  default.
+  EOF
 
   validation {
     condition = (
