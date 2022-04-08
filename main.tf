41b553347a5d7252bc40901c9d975ce939faca94
anniehedgpeth <annie@hashicorp.com>
2022-04-08 16:48:28 -0600

    test

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ module "database" {
 # TFE and Replicated settings to pass to the tfe_init module
 # -----------------------------------------------------------------------------
 module "settings" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/settings?ref=main"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/settings?ref=gcp_init_utilities"
 
   # TFE Base Configuration
   installation_type = var.installation_type
@@ -185,7 +185,7 @@ module "settings" {
 # Azure user data / cloud init used to install and configure TFE on instance(s)
 # -----------------------------------------------------------------------------
 module "tfe_init" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/tfe_init?ref=main"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/tfe_init?ref=gcp_init_utilities"
 
   # TFE & Replicated Configuration data
   cloud                    = "azurerm"
