7a411d8876d65c7868a29634d19a14ab12679389
anniehedgpeth <annie@hashicorp.com>
2022-04-08 17:38:48 -0600

    add production_type to tfe init

--- main.tf
+++ main.tf
@@ -190,6 +190,7 @@ module "tfe_init" {
   # TFE & Replicated Configuration data
   cloud                    = "azurerm"
   distribution             = var.distribution
+  production_type          = var.production_type
   tfe_configuration        = module.settings.tfe_configuration
   replicated_configuration = module.settings.replicated_configuration
   airgap_url               = var.airgap_url
