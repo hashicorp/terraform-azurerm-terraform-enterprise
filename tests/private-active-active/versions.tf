5e2fa75fc5dd9ffa9f28ae20cdd181b649f67e2f
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2022-04-21 15:28:23 -0400

    Initial commit to update Terraform and the AzureRM provider

--- tests/private-active-active/versions.tf
+++ tests/private-active-active/versions.tf
@@ -7,18 +7,20 @@ terraform {
     }
   }
 
-  required_version = ">= 0.13"
+  required_version = ">= 1.1.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.79"
+      version = "~> 3.2.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0.0"
+      version = "~> 3.1.0"
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
