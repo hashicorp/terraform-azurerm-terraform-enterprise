863eb43631735ebe6aa1b0a1210ba2ec8e4b6fee
anniehedgpeth <annie@hashicorp.com>
2023-02-21 15:10:31 -0700

    remove tbw_image

--- variables.tf
+++ variables.tf
@@ -864,25 +864,6 @@ variable "metrics_endpoint_port_https" {
   EOD
 }
 
-variable "tbw_image" {
-  default     = null
-  type        = string
-  description = <<-EOD
-  Set this to 'custom_image' if you want to use an alternative Terraform build worker image,
-  and use the 'custom_image_tag' variable to define its name and tag.
-  Default is 'default_image'. 
-  EOD
-
-  validation {
-    condition = (
-      var.tbw_image == "default_image" ||
-      var.tbw_image == "custom_image" ||
-      var.tbw_image == null
-    )
-    error_message = "The tbw_image must be 'default_image', 'custom_image', or null. If left unset, TFE will default to 'default_image'."
-  }
-}
-
 variable "trusted_proxies" {
   default     = []
   description = <<-EOD
