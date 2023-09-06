f939822b2ebac325c9780130a93451d443d047b2
anniehedgpeth <annie@hashicorp.com>
2023-09-06 12:31:36 -0600

    update standalone test providers

--- tests/standalone-mounted-disk/versions.tf
+++ tests/standalone-mounted-disk/versions.tf
@@ -2,6 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+  backend "remote" {
+    organization = "terraform-enterprise-modules-test"
+
+    workspaces {
+      name = "azure-standalone-mounted-disk"
+    }
+  }
+
   required_version = "~> 1.0"
   required_providers {
     azurerm = {
