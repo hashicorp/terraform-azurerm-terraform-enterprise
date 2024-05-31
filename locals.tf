d939a2a8e2edc72303c9097d702999896243c874
Miguel Hernández <miguel.hernandez@hashicorp.com>
2024-05-31 12:27:06 +0200

    add active-active as valid production_type

--- locals.tf
+++ locals.tf
@@ -6,9 +6,8 @@
 locals {
   # TFE Architecture
   # ----------------
-  # Determine whether or not TFE in active-active mode based on node count, by default standalone is assumed
-  active_active = var.vm_node_count >= 2 ? true : false
-  disk_mode     = var.production_type == "disk" ? true : false
+  active_active = var.vm_node_count >= 2 || var.production_type == "active-active"
+  disk_mode     = var.production_type == "disk"
 
   # Network
   # -------
