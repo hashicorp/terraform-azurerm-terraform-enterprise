9f204efc0c9c236912a94ee405afb9545ad0511d
Hamza Baig <hamza.baig@hashicorp.com>
2024-07-12 13:16:16 -0700

    flag to stop azure postgres errors

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -11,7 +11,8 @@ resource "azurerm_postgresql_flexible_server" "tfe" {
   location            = var.location
   name                = "${var.friendly_name_prefix}-pg"
   resource_group_name = var.resource_group_name
-
+  public_network_access_enabled = var.public_network_access_enabled
+  administrator_login    = var.database_user
   administrator_login    = var.database_user
   administrator_password = random_string.tfe_pg_password.result
   backup_retention_days  = var.database_backup_retention_days
