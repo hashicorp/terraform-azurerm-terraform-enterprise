196b0875f5d14e9ba01824b7d4cc35e26fd9c8c9
Hamza Baig <hamza.baig@hashicorp.com>
2024-07-12 13:24:54 -0700

    removing dup administrator_password

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -13,7 +13,6 @@ resource "azurerm_postgresql_flexible_server" "tfe" {
   resource_group_name = var.resource_group_name
   public_network_access_enabled = var.public_network_access_enabled
   administrator_login    = var.database_user
-  administrator_login    = var.database_user
   administrator_password = random_string.tfe_pg_password.result
   backup_retention_days  = var.database_backup_retention_days
   delegated_subnet_id    = var.database_subnet_id
