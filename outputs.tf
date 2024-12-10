efc4703f4c474adb67f2cbcf23a378adf9987933
kumarkavish <kavish.kumar@hashicorp.com>
2024-12-10 18:18:55 +0530

    TF-11115, TF-11117, TF-11116] tfectl support bundle on k8s cluster with AA operational mode

--- outputs.tf
+++ outputs.tf
@@ -52,7 +52,7 @@ output "storage_account_container_name" {
 }
 
 output "storage_account_primary_blob_connection_string" {
-  value       = module.object_storage.storage_account_primary_blob_connection_string
+  value       = local.object_storage.storage_account_primary_blob_connection_string
   description = "The connection string associated with the primary location for the storage account used by TFE"
 }
 
