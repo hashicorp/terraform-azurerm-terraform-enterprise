034b50777eeb48269e67d032eb6882ca1b6b7fcc
Sripal K Jain <paljain4444@gmail.com>
2025-09-30 21:42:59 +0530

    updated azurerm version contraint for testing

--- versions.tf
+++ versions.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3"
+      version = ">=3.2"
     }
     tls = {
       source  = "hashicorp/tls"
