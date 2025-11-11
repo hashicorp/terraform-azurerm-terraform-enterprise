67add342bdc57afe57cc02471746779bff59f974
Kaushiki Anand <kaushikianand@Kaushikis-MacBook-Pro.local>
2025-11-11 16:20:24 +0530

    for manual testing

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -213,7 +213,8 @@ resource "azurerm_application_gateway" "tfe_ag" {
     content {
       name                          = local.frontend_ip_configuration_name_private
       subnet_id                     = var.network_frontend_subnet_id
-      private_ip_address_allocation = "Dynamic"
+      private_ip_address_allocation = "Static"
+      private_ip_address            = local.private_ip_address
     }
   }
 
