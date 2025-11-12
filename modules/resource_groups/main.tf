603f8c16bc24c0303d9884e45c7846f010eae794
Tauhid <tauhidanjum@gmail.com>
2025-11-12 11:05:46 +0530

    using the branch

--- modules/resource_groups/main.tf
+++ modules/resource_groups/main.tf
@@ -8,15 +8,15 @@ locals {
   #   by default a new resource group will be created and used
   #   additionally, the same resource group will be used for dns
   #   and kv if alternative resource group names are not supplied
-  resource_group_name     = var.resource_group_name == null ? azurerm_resource_group.tfe_resource_group[0].name : var.resource_group_name
-  resource_group_name_dns = var.resource_group_name_dns == null ? local.resource_group_name : var.resource_group_name_dns
+  resource_group_name     = var.resource_group_name 
+  resource_group_name_dns = var.resource_group_name_dns
 }
 
-resource "azurerm_resource_group" "tfe_resource_group" {
-  count = var.resource_group_name == null ? 1 : 0
+# resource "azurerm_resource_group" "tfe_resource_group" {
+#   count = var.resource_group_name == null ? 1 : 0
 
-  name     = "${var.friendly_name_prefix}-rg"
-  location = var.location
+#   name     = "${var.friendly_name_prefix}-rg"
+#   location = var.location
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
